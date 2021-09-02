aa979dfa2ca153d25de7031ad5b93798ccb1364e
andriydobrosynets <andriy.andriy1995@gmail.com>
2021-09-02 15:06:41 +0300

    Lesson 2 HW

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import './App.css';
+import Users from './components/users/Users';
+import Posts from './components/posts/Posts';
+import Comments from "./components/comments/Comments";
 
 function App() {
   return (
     <div>
-
+      <Users/>
+        <h3>Users Posts</h3>
+        <Posts/>
+      <h3>Сomments to posts</h3>
+      <Comments/>
     </div>
   );
 }
-
 export default App;
